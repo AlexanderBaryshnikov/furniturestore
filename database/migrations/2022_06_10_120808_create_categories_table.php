53e090ca170de34212352b0150063db3f85c8d25
AlexanderBaryshnikov <69297560+AlexanderBaryshnikov@users.noreply.github.com>
2022-06-10 15:28:45 +0300

    Add categories (#7)

---
+++ database/migrations/2022_06_10_120808_create_categories_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('categories', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 255)->default('');
+            $table->string('slug', 255);
+            $table->unsignedBigInteger('parent_id')->default(0);
+            $table->boolean('is_subcategory')->default(false);
+            $table->timestamps();
+            $table->softDeletes();
+
+            $table->unique('slug');
+            $table->index('parent_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('categories');
+    }
+};

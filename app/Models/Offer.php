4448e93c9bb0134e929fd261281fa1687071c29b
AlexanderBaryshnikov <axr.baryshnikov@gmail.com>
2022-06-19 17:07:14 +0300

    Add action nova for offer properties

--- app/Models/Offer.php
+++ app/Models/Offer.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Cviebrock\EloquentSluggable\Sluggable;
+use Illuminate\Support\Str;
 
 class Offer extends Model
 {
@@ -32,5 +33,38 @@ class Offer extends Model
     public function product()
     {
         return $this->belongsTo(Product::class);
+    }
+
+    public static function getAllPropertiesList()
+    {
+        return self::parseProperties(self::getAllProperties());
+    }
+
+    public static function parseProperties(string $properties = '')
+    {
+        $property_values = Str::replace(['[', '"', ']'], '', $properties);
+
+        return collect(explode(',', $property_values));
+    }
+
+    public static function getAllProperties(int $product_id = 0): string
+    {
+        $builder = self::select('properties');
+
+        if ($product_id) {
+            $builder->where('product_id', $product_id);
+        }
+
+        $properties = $builder->get();
+
+        if (!$properties->count()) {
+            return '';
+        }
+
+        $p = $properties->map(function ($property) {
+            return $property->properties;
+        });
+
+        return implode(',',$p->toArray());
     }
 }

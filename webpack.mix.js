c7b91961c0a030e98a721458bb824cf08094096d
AlexanderBaryshnikov <axr.baryshnikov@gmail.com>
2022-06-21 15:19:18 +0300

    Webpack mix

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,7 +11,33 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-    .postCss('resources/css/app.css', 'public/css', [
-        //
-    ]);
+mix.js('src/js/script.js', 'js')
+    .copy('src/js/bootstrap.bundle.min.js', 'public/js/bootstrap.bundle.min.js')
+    .copy('src/js/countdon.min.js', 'public/js/countdon.min.js')
+    .copy('src/js/jquery.meanmenu.js', 'public/js/jquery.meanmenu.js')
+    .copy('src/js/jquery.nicescroll.min.js', 'public/js/jquery.nicescroll.min.js')
+    .copy('src/js/jquery.treeview.js', 'public/js/jquery.treeview.js')
+    .copy('src/js/jquery-ui.min.js', 'public/js/jquery-ui.min.js')
+    .copy('src/js/lightbox.min.js', 'public/js/lightbox.min.js')
+    .copy('src/js/plugins.js', 'public/js/plugins.js')
+    .copy('src/js/slick.min.js', 'public/js/slick.min.js')
+    .copy('src/js/wow.min.js', 'public/js/wow.min.js')
+    .copy('src/js/vendor/jquery-3.6.0.min.js', 'public/js/vendor/jquery-3.6.0.min.js')
+    .copy('src/js/vendor/jquery-migrate-3.3.2.min.js', 'public/js/vendor/jquery-migrate-3.3.2.min.js')
+    .copy('src/js/vendor/modernizr-3.11.2.min.js', 'public/js/vendor/modernizr-3.11.2.min.js')
+    .styles([
+        'src/css/bootstrap.min.css',
+        'src/css/animate.min.css',
+        'src/css/default.css',
+        'src/css/jquery-ui.css',
+        'src/css/lightbox.min.css',
+        'src/css/material-design-iconic-font.css',
+        'src/css/meanmenu.min.css',
+        'src/css/responsive.css',
+        'src/css/shortcode.css',
+        'src/css/slick.min.css',
+        'src/css/style.css',
+    ], 'public/css/style.css')
+    .copyDirectory('src/img', 'public/img')
+    .copyDirectory('src/fonts', 'public/fonts')
+    .copyDirectory('src/css/images', 'public/css/images');

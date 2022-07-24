9fec9ff1c6b1fdbfecf04249023ceb2e817ff4f9
AlexanderBaryshnikov <69297560+AlexanderBaryshnikov@users.noreply.github.com>
2022-07-24 17:47:27 +0300

    Service CustomPathGenerator for MediaLibrary (#27)

---
+++ app/Services/MediaLibrary/CustomPathGenerator.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Services\MediaLibrary;
+
+use Illuminate\Support\Str;
+use Spatie\MediaLibrary\MediaCollections\Models\Media;
+use Spatie\MediaLibrary\Support\PathGenerator\PathGenerator;
+
+class CustomPathGenerator implements PathGenerator
+{
+    /*
+     * Get the path for the given media, relative to the root storage path.
+     */
+    public function getPath(Media $media): string
+    {
+        return Str::lower(substr(strrchr($media->model_type, '\\'), 1) . '/' . $media->id . '/');
+    }
+
+    /*
+     * Get the path for conversions of the given media, relative to the root storage path.
+     */
+    public function getPathForConversions(Media $media): string
+    {
+        return $this->getPath($media) . '/conversions/';
+    }
+
+    /*
+     * Get the path for responsive images of the given media, relative to the root storage path.
+     */
+    public function getPathForResponsiveImages(Media $media): string
+    {
+        return $this->getPath($media).'/responsive-images/';
+    }
+}

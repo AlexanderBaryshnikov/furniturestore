b17a5fa8de423c4771e88a8ebaef5b4e74c08c32
AlexanderBaryshnikov <axr.baryshnikov@gmail.com>
2022-09-01 16:39:52 +0300

    Add label for offer

--- resources/views/partials/offer/main-photo/index.blade.php
+++ resources/views/partials/offer/main-photo/index.blade.php
@@ -1,7 +1,10 @@
 @if($offer->getMedia('offers')->count())
-    <div class="single-pro-slider single-big-photo view-lightbox slider-for">
-        @foreach($offer->getMedia('offers') as $photo)
-            @include('partials.offer.main-photo.item.index')
-        @endforeach
+    <div class="position-relative">
+        @include('partials.product.block.labels.index')
+        <div class="single-pro-slider single-big-photo view-lightbox slider-for">
+            @foreach($offer->getMedia('offers') as $photo)
+                @include('partials.offer.main-photo.item.index')
+            @endforeach
+        </div>
     </div>
 @endif
